0115c5a5b2ddf6aa653abef977b6f91ad11be264
Ivan <ivan1501@gmail.com>
2020-06-18 11:09:07 +0200

    Created ToDo component.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TodoList,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ul>
+        <li> Todo 1 </li> 
+        <li> Todo 2 </li> 
+        <li> Todo 3 </li> 
+        <li> Todo 4 </li> 
+    </ul> 
+  </div>
+</template>
+
+<script type = "text/javascript" >
+
+export default {
+};
+</script>
+<style>
+</style>
