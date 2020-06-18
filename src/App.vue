a02bcbedb451148484eefb935424a4cbf205555a
Ivan <ivan1501@gmail.com>
2020-06-18 11:14:40 +0200

    Added data to component.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <todo-list></todo-list>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
@@ -8,10 +8,34 @@
 import TodoList from './components/TodoList';
 
 export default {
+  name: 'app',
   components: {
-    TodoList,
+  TodoList,
+  },
+
+data() {
+    return {
+      todos: [{
+        title: 'Todo 1',
+        project: 'Project 1',
+        done: false,
+      }, {
+        title: 'Todo 2',
+        project: 'Project 2',
+        done: true,
+      }, {
+        title: 'Todo 3',
+        project: 'Project 3',
+        done: false,
+      }, {
+        title: 'Todo 4',
+        project: 'Project 4',
+        done: false,
+      }],
+    };
   },
 };
+
 </script>
 
 <style>
