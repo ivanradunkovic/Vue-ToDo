a7bab8d635d3eebc35a5c03793fbcfea8c8b9275
Ivan <ivan1501@gmail.com>
2020-06-26 11:12:19 +0200

    ToDo component created.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,18 +1,44 @@
 <template>
   <div>
-    <ul>
-        <li> Todo 1 </li> 
-        <li> Todo 2 </li> 
-        <li> Todo 3 </li> 
-        <li> Todo 4 </li> 
-    </ul> 
+    <p class="tasks">Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p class="tasks">Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
   </div>
 </template>
 
 <script type = "text/javascript" >
-
+import sweetalert from 'sweetalert';
 export default {
+  props: ['todos'],
+  components: {
+  },
+  methods: {
+    deleteTodo(todo) {
+      sweetalert({
+        title: 'Are you sure?',
+        text: 'This To-Do will be permanently deleted!',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#DD6B55',
+        confirmButtonText: 'Yes, delete it!',
+        closeOnConfirm: false,
+      },
+      () => {
+        const todoIndex = this.todos.indexOf(todo);
+        this.todos.splice(todoIndex, 1);
+        sweetalert('Deleted!', 'Your To-Do has been deleted.', 'success');
+      });
+    },
+    completeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos[todoIndex].done = true;
+      sweetalert('Success!', 'To-Do completed!', 'success');
+    },
+  },
 };
 </script>
-<style>
+
+<style scoped>
+p.tasks {
+  text-align: center;
+}
 </style>
